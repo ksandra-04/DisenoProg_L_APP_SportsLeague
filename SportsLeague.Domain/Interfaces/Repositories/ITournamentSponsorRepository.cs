7b710e895b6b1226b35562635a7421fb99c879b6
ksandra-04 <kasandrapimienta1118030@correo.itm.edu.co>
2026-04-06 17:51:56 -0500

    Add sponsor category enum, sponsor and tournament sponsor entities, update Tournament.cs, and create repositories and service interfaces

---
+++ SportsLeague.Domain/Interfaces/Repositories/ITournamentSponsorRepository.cs
@@ -0,0 +1,11 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Repositories
+{
+    public interface ITournamentSponsorRepository : IGenericRepository<TournamentSponsor>
+    {
+        Task<IEnumerable<TournamentSponsor>> GetByTournamentIdAsync(int tournamentId);
+        Task<IEnumerable<TournamentSponsor>> GetBySponsorIdAsync(int sponsorId);
+        Task<TournamentSponsor?> GetByTournamentAndSponsorAsync(int tournamentId, int sponsorId);
+    }
+}
---
+++ SportsLeague.Domain/Interfaces/Services/ISponsorService.cs
@@ -0,0 +1,18 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Services
+{
+    public interface ISponsorService
+    {
+        Task<IEnumerable<Sponsor>> GetAllAsync();
+        Task<Sponsor?> GetByIdAsync(int id);
+        Task<Sponsor> CreateAsync(Sponsor sponsor);
+        Task UpdateAsync(int id, Sponsor sponsor);
+        Task DeleteAsync(int id);
+
+        // Vinculación con torneos
+        Task<TournamentSponsor> AddToTournamentAsync(int sponsorId, int tournamentId, decimal contractAmount);
+        Task<IEnumerable<TournamentSponsor>> GetTournamentsBySponsorsAsync(int sponsorId);
+        Task RemoveFromTournamentAsync(int sponsorId, int tournamentId);
+    }
+}
